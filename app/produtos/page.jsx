a699b7b2c147e046343bc7efaa6f0c740715559e
Alexandre Ribeiro de Sá <alexribeirodesa@gmail.com>
2023-11-20 15:17:21 -0300

    Update page.jsx

--- app/produtos/page.jsx
+++ app/produtos/page.jsx
@@ -1,6 +1,6 @@
 import fs from 'fs'
-import Title from "../../../components/title";
-import Section from "../../../components/section";
+import Title from "../../components/title";
+import Section from "../../components/section";
 
 export default function ProductsPage() {
 	return(
