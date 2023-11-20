2049cd3f2f29069af7d9013b562477c0cb4c7934
Alexandre Ribeiro de Sá <alexribeirodesa@gmail.com>
2023-11-20 12:57:01 -0300

    Update page.jsx

--- app/produtos/page.jsx
+++ app/produtos/page.jsx
@@ -63,7 +63,7 @@ export default async function ProductsPage() {
 			{/* TITLE */}
 			<Title>PRODUTOS</Title>
 			<div>
-				{JSON.stringify(data)}
+				{/*JSON.stringify(data)*/}
 			</div>
 		</>
 	)
