a4c4ccc9d71466b98195b2f6c29b113167d9cfc4
Alexandre Ribeiro de Sá <alexribeirodesa@gmail.com>
2023-11-23 01:40:13 -0300

    Update product_card.jsx

--- app/produtos/product_card.jsx
+++ app/produtos/product_card.jsx
@@ -2,7 +2,7 @@ export default function ProductCard({children, img}) {
 	return (
 		<>
 			<div className='dui-card min-h-full bg-base-100 border border-solid'>
-				<figure className='max-h-32 overflow-hidden'><img className='min-h-[8rem]' src={img} /></figure>
+				<figure className='max-h-32 overflow-hidden'><img className='min-h-[8rem]' src={img} loading="lazy" /></figure>
 				<div className='flex dui-card-body p-4'>
 					{children}
 				</div>
