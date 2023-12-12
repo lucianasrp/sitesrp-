72109b85a5c1f746304aea93afa7eb53e4f310b6
Alexandre Ribeiro de Sá <alexribeirodesa@gmail.com>
2023-12-11 23:37:57 -0300

    Update company.js

--- config/company.js
+++ config/company.js
@@ -1,7 +1,7 @@
 const COMPANY = {
 	name: 'SRP Sartori Revert Pisos',
 	//cnpj: '12.345.678/0001-01',
-	worktime: 'seg a sex das 8:00 às 18:00',
+	worktime: 'Seg a Sex das 8:00 às 18:00',
 	location: {
 		address: 'Rua Alaélcio Pereira de Araujo, 449',
 		neighborhood: 'Mirante',
