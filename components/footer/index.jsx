4adb928006694db66ebf19a1b945a3fbaa530e85
Alexandre Ribeiro de Sá <alexribeirodesa@gmail.com>
2023-12-11 22:47:36 -0300

    + horario de funcionamento

--- components/footer/index.jsx
+++ components/footer/index.jsx
@@ -24,6 +24,7 @@ export default function Footer({ className, src, children }) {
 							<div className='flex gap-2'>
 								<svg xmlns='http://www.w3.org/2000/svg' width='1rem' height='1rem' className='fill-current' viewBox='0 0 384 512'><path d='M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z' /></svg>
 								<p>{COMPANY.location.address}<br />{COMPANY.location.neighborhood} - {COMPANY.location.city}, {COMPANY.location.state}</p>
+								<p>{COMPANY.worktime}</p>
 							</div>
 
 							{/* desenha numeros de telefone */}
